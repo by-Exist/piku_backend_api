cc38d3f83ea46521438e13b8471fbe06f71b77e2
Exist <bolk9652@naver.com>
2021-02-21 15:32:45 +0900

    ✨feature: Basic API Document - Swagger

--- backend/settings/common.py
+++ backend/settings/common.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third apps
+    "drf_yasg",
     "rest_framework",
     # Local apps
     "accountapp",
--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 from rest_framework_nested import routers
 from rest_framework_simplejwt import views as simplejwt_views
 from accountapp import views as accountapp_views
@@ -35,8 +38,39 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Dev Backend API",
+            default_version="v1",
+            description="백엔드 개발 환경에서 활용될 API 문서입니다.",
+            # terms_of_service="https://www.google.com/policies/terms/",
+            # contact=openapi.Contact(email="contact@snippets.local"),
+            # license=openapi.License(name="BSD License"),
+        ),
+        # 사용자가 접근 가능한 엔드포인트만 보여주고 싶다면 False로 설정
+        public=True,
+        # authentication_classes=(),
+        permission_classes=(permissions.AllowAny,),
+    )
+
     urlpatterns = [
         *urlpatterns,
         *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
         *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+        re_path(
+            r"^swagger(?P<format>\.json|\.yaml)$",
+            schema_view.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        re_path(
+            r"^swagger/$",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        re_path(
+            r"^redoc/$",
+            schema_view.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
     ]
