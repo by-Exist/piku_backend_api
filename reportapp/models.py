ff0dab3b5fb287e8e24a840fce64c6c8f9ad7111
Jongbeom-Kwon <bolk9652@naver.com>
2021-03-09 16:31:53 +0900

    🐞fix: ReportModel reasom field
    integer choices는 숫자로 평가되어 어떤 용도로 사용하는지 스키마만으로 활용하기 어려웠다.
    CharField + TextChoices로 변경하여 schema에서 어떤 용도인지 대략적으로 파악할 수 있도록 변경하였다.

--- reportapp/models.py
+++ reportapp/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth import get_user_model
 
 # Report Model
 class AbstractReport(models.Model):
-    class CommonReason(models.IntegerChoices):
-        ADULT = 1, "성적 요소가 포함되어 있습니다."
-        HATE = 2, "증오/혐오 등의 내용이 포함되어 있습니다."
-        SPAM = 3, "스팸 및 광고성 내용이 포함되어 있습니다."
-        CHILD_ABUSE = 4, "아동학대 관련 내용이 포함되어 있습니다."
-        OTHER = 5, "기타"
+    class CommonReason(models.TextChoices):
+        ADULT = "Sex", "성적 요소가 포함되어 있습니다."
+        HATE = "Hate", "증오/혐오 등의 내용이 포함되어 있습니다."
+        SPAM = "Spam", "스팸 및 광고성 내용이 포함되어 있습니다."
+        CHILD_ABUSE = "Child", "아동학대 관련 내용이 포함되어 있습니다."
+        OTHER = "Other", "기타"
 
     reporter = models.ForeignKey(
         get_user_model(),
@@ -19,7 +19,7 @@ class AbstractReport(models.Model):
         on_delete=models.SET_NULL,
         verbose_name="신고자",
     )
-    reason = models.PositiveSmallIntegerField("신고 사유", choices=CommonReason.choices)
+    reason = models.CharField("신고 사유", max_length=15, choices=CommonReason.choices)
     body = models.CharField("신고 내용", blank=True, max_length=511)
     image = models.ImageField(
         "증빙 사진", blank=True, upload_to="reportapp/report/%Y/%m/%d"
