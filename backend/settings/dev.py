6459aa4fadf36d8e7cf13659cb0b8bdf64f3a8b6
Jongbeom-Kwon <bolk9652@naver.com>
2021-03-14 21:20:23 +0900

    ✨feature: Drf-spectacular setting
    COMPONENT_SPLIT_REQUEST 속성을 True로 변경함으로써 put, patch에서 발생하던 문제를 해결하였다.
    기타 사용할 만한 여러 setting을 기록하였다.

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -51,3 +51,16 @@ INTERNAL_IPS = [socket.gethostbyname(socket.gethostname())[:-1] + "1"]
 # LOCAL EMAIL - https://docs.djangoproject.com/en/3.1/topics/email/#file-backend
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR / PROJECT_NAME / "_email"
+
+
+# Dev Drf_spectacular Settings
+SPECTACULAR_SETTINGS = {
+    # Schema metadata
+    "TITLE": "Developer's Swagger",
+    "DESCRIPTION": "개발 용도로 사용되는 Swagger입니다.",
+    "VERSION": "0.0.1",
+    # Useful settings
+    "SERVE_INCLUDE_SCHEMA": False,
+    "COMPONENT_SPLIT_REQUEST": True,
+    "DISABLE_ERRORS_AND_WARNINGS": False,
+}
