b5730f56373d53b8646befc662f0e92cfa0b39d0
Jongbeom-Kwon <bolk9652@naver.com>
2021-03-16 18:21:35 +0900

    ✨feature: Tokenapp Schema Create
    Tokenapp의 Schema를 기록하였다.
    extend_schema에 settings의 값을 넣는 발상은 좋은 듯 하다. 이 방식을 더 사용할 수 있는 장소가 있을까?

--- tokenapp/views.py
+++ tokenapp/views.py
@@ -1,11 +1,35 @@
-from rest_framework_simplejwt import views as simplejwt_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.settings import api_settings
+from drf_spectacular.utils import extend_schema
 
 
-class DecoratedTokenObtainPairView(simplejwt_views.TokenObtainPairView):
+class DecoratedTokenObtainPairView(TokenObtainPairView):
+    @extend_schema(
+        description="\n\n".join(
+            [
+                "## [ Description ]",
+                "- Obtain Token",
+                f"- Access Token Lifetime = {api_settings.ACCESS_TOKEN_LIFETIME}",
+                f"- Refresh Token Lifetime = {api_settings.REFRESH_TOKEN_LIFETIME}",
+                "## [ Permission ]",
+                "- AllowAny",
+            ]
+        )
+    )
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
 
 
-class DecoratedTokenRefreshView(simplejwt_views.TokenRefreshView):
+class DecoratedTokenRefreshView(TokenRefreshView):
+    @extend_schema(
+        description="\n\n".join(
+            [
+                "## [ Description ]",
+                "- Refresh Token",
+                "## [ Permission ]",
+                "- AllowAny",
+            ]
+        )
+    )
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
