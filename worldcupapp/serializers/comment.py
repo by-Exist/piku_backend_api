3e9043f9b58037f9b4d6b8c9f351ba2f7692acb1
Jongbeom-Kwon <bolk9652@naver.com>
2021-03-22 22:32:51 +0900

    ✨feature: Comment - ViewSet, Serializer
    ViewSet과 Serializer의 작업이 일차적으로 끝이 났다.

--- worldcupapp/serializers/comment.py
+++ worldcupapp/serializers/comment.py
@@ -1,21 +1,26 @@
 from rest_framework import serializers
 from rest_framework_nested.serializers import NestedHyperlinkedIdentityField
-from ..models import AuthUserComment
+from rest_polymorphic.serializers import PolymorphicSerializer
+from ..models import AuthUserComment, AnonUserComment, Profile
 
 
 class AuthUserCommentDetailSerializer(serializers.ModelSerializer):
+
+    nickname = serializers.CharField(read_only=True, source="writer.profile.nickname")
+    avatar = serializers.CharField(read_only=True, source="writer.profile.avatar")
+
     class Meta:
         model = AuthUserComment
         fields = [
             "id",
-            "writer",
+            "nickname",
+            "avatar",
             "media",
             "body",
             "created_at",
             "updated_at",
         ]
         extra_kwargs = {
-            "writer": {"read_only": True},
             "media": {"read_only": True},
         }
 
@@ -27,6 +32,8 @@ class AuthUserCommentListSerializer(serializers.ModelSerializer):
         view_name="comment-detail",
         parent_lookup_kwargs={"worldcup_pk": "worldcup__pk"},
     )
+    nickname = serializers.CharField(read_only=True, source="writer.profile.nickname")
+    avatar = serializers.CharField(read_only=True, source="writer.profile.avatar")
 
     class Meta:
         model = AuthUserComment
@@ -34,14 +41,14 @@ class AuthUserCommentListSerializer(serializers.ModelSerializer):
             "id",
             "url",
             "media_id",
-            "writer",
+            "nickname",
+            "avatar",
             "media",
             "body",
             "created_at",
             "updated_at",
         ]
         extra_kwargs = {
-            "writer": {"read_only": True},
             "media": {"read_only": True},
         }
 
@@ -63,3 +70,99 @@ class AuthUserCommentListSerializer(serializers.ModelSerializer):
             media = self.context["view"].parent_object.media_set.get(pk=media_id)
             validated_data |= {"media": media}
         return super().create(validated_data)
+
+
+class AnonUserCommentDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnonUserComment
+        fields = [
+            "id",
+            "anon_nickname",
+            "media",
+            "body",
+            "created_at",
+            "updated_at",
+        ]
+        extra_kwargs = {
+            "media": {"read_only": True},
+            "anon_nickname": {"read_only": True},
+        }
+
+
+class AnonUserCommentListSerializer(serializers.ModelSerializer):
+
+    media_id = serializers.CharField(write_only=True, required=False, allow_null=True)
+    url = NestedHyperlinkedIdentityField(
+        view_name="comment-detail",
+        parent_lookup_kwargs={"worldcup_pk": "worldcup__pk"},
+    )
+
+    class Meta:
+        model = AnonUserComment
+        fields = [
+            "id",
+            "url",
+            "media_id",
+            "anon_nickname",
+            "anon_password",
+            "media",
+            "body",
+            "created_at",
+            "updated_at",
+        ]
+        extra_kwargs = {
+            "media": {"read_only": True},
+            "anon_password": {
+                "write_only": True,
+                "style": {"input_type": "password"},
+            },
+        }
+
+    def validate_media_id(self, media_id):
+        if not media_id:
+            return media_id
+        media_set = self.context["view"].parent_object.media_set
+        if media_id and (not media_set.filter(pk=media_id).exists()):
+            raise serializers.ValidationError("월드컵에 해당 미디어가 존재하지 않습니다.")
+        return media_id
+
+    def create(self, validated_data):
+        validated_data |= {
+            "worldcup": self.context["view"].parent_object,
+        }
+        media_id = validated_data.pop("media_id", None)
+        if media_id:
+            media = self.context["view"].parent_object.media_set.get(pk=media_id)
+            validated_data |= {"media": media}
+        return super().create(validated_data)
+
+
+class AnonUserCommentPasswordCheckSerializer(serializers.Serializer):
+
+    password = serializers.CharField(style={"input_type": "password"})
+
+    def validate_password(self, password):
+        comment = self.context["view"].get_object()
+        if not (comment.anon_password == password):
+            raise serializers.ValidationError("password가 일치하지 않습니다.")
+        return password
+
+
+class CommentPolymorphicDetailSerializer(PolymorphicSerializer):
+
+    resource_type_field_name = "usertype"
+
+    model_serializer_mapping = {
+        AuthUserComment: AuthUserCommentDetailSerializer,
+        AnonUserComment: AnonUserCommentDetailSerializer,
+    }
+
+
+class CommentPolymorphicListSerializer(PolymorphicSerializer):
+
+    resource_type_field_name = "usertype"
+
+    model_serializer_mapping = {
+        AuthUserComment: AuthUserCommentListSerializer,
+        AnonUserComment: AnonUserCommentListSerializer,
+    }
--- worldcupapp/views/comment.py
+++ worldcupapp/views/comment.py
@@ -1,11 +1,15 @@
 from django.utils.functional import cached_property
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from drf_patchonly_mixin import mixins as dpm_mixins
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from ..models import Comment, Worldcup
 from ..serializers import (
-    AuthUserCommentListSerializer,
-    AuthUserCommentDetailSerializer,
+    AnonUserCommentPasswordCheckSerializer,
+    CommentPolymorphicDetailSerializer,
+    CommentPolymorphicListSerializer,
 )
 
 
@@ -17,10 +21,11 @@ class CommentViewSet(
     viewsets.GenericViewSet,
 ):
 
-    serializer_class = AuthUserCommentDetailSerializer
+    serializer_class = CommentPolymorphicDetailSerializer
     serializer_action_class = {
-        "list": AuthUserCommentListSerializer,
-        "create": AuthUserCommentListSerializer,
+        "list": CommentPolymorphicListSerializer,
+        "create": CommentPolymorphicListSerializer,
+        "check_password": AnonUserCommentPasswordCheckSerializer,
     }
 
     @cached_property
@@ -34,3 +39,74 @@ class CommentViewSet(
         if serializer_class := self.serializer_action_class.get(self.action, None):
             return serializer_class
         return self.serializer_class
+
+    @action(methods=["post"], detail=True, url_path="password/check")
+    def check_password(self, request, pk=None, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+CommentViewSet = extend_schema_view(
+    list=extend_schema(
+        description="\n\n".join(
+            [
+                "## [ Description ]",
+                "- Worldcup's Comment List",
+                "## [ Permission ]",
+                "- AllowAny",
+            ]
+        ),
+    ),
+    create=extend_schema(
+        description="\n\n".join(
+            [
+                "## [ Description ]",
+                "- Worldcup's Comment Create",
+                "## [ Permission ]",
+                "- AllowAny",
+            ]
+        ),
+    ),
+    partial_update=extend_schema(
+        description="\n\n".join(
+            [
+                "## [ Description ]",
+                "- Worldcup's Comment Partial Update",
+                "- user가 anonymous일 때, 해당 엔드포인트는 check_password 엔드포인트와 함께 사용되어야 한다.",
+                "## [ Permission ]",
+                "- authenticated = IsWriter",
+                "- anonymous = AllowAny",
+            ]
+        ),
+    ),
+    destroy=extend_schema(
+        description="\n\n".join(
+            [
+                "## [ Description ]",
+                "- Worldcup's Comment Destroy",
+                "- user가 anonymous일 때, 해당 엔드포인트는 check_password 엔드포인트와 함께 사용되어야 한다.",
+                "## [ Permission ]",
+                "- authenticated = IsWriter",
+                "- anonymous = AllowAny",
+            ]
+        ),
+    ),
+    check_password=extend_schema(
+        description="\n\n".join(
+            [
+                "## [ Description ]",
+                "- Worldcup's Comment Check Password",
+                "- 익명 유저의 partial_update, destroy와 동시에 사용되어야 하는 엔드포인트.",
+                "- password를 입력받아 해당 익명 댓글의 비밀번호와 동일한지 확인한다.",
+                "## [ Permission ]",
+                "- Anonymous",
+            ]
+        ),
+        responses={
+            204: Response(status=status.HTTP_204_NO_CONTENT),
+            400: Response(status=status.HTTP_400_BAD_REQUEST),
+        },
+    ),
+)(CommentViewSet)
