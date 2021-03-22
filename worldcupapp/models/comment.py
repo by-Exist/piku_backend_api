5c2060fc066fc0c6aa0e86f19640285c775c3f8e
Jongbeom-Kwon <bolk9652@naver.com>
2021-03-22 19:05:00 +0900

    ✨feature: Comment models
    Polymorphic을 활용하여 Comment 모델을 정의하였다.

--- worldcupapp/models/comment.py
+++ worldcupapp/models/comment.py
@@ -1,27 +1,38 @@
-# class Comment(models.Model):
-#     writer = models.ForeignKey(
-#         get_user_model(),
-#         null=True,
-#         blank=True,
-#         on_delete=models.CASCADE,
-#         verbose_name="작성자",
-#     )
-#     anonymous_nickname = models.CharField(
-#         "익명 닉네임",
-#         default="익명",
-#         max_length=13,
-#     )
-#     anonymous_password = models.CharField(
-#         "익명 패스워드",
-#         max_length=15,
-#     )
-#     worldcup = models.ForeignKey(Worldcup, on_delete=models.CASCADE, verbose_name="월드컵")
-#     media = models.ForeignKey(
-#         BaseMedia, null=True, blank=True, on_delete=models.CASCADE, verbose_name="미디어"
-#     )
-#     comment = models.CharField("댓글 내용", max_length=511)
-#     created_at = models.DateTimeField("작성시각", auto_now_add=True)
-#     updated_at = models.DateTimeField("수정시각", auto_now=True)
+from copy import deepcopy
+from django.db import models
+from django.contrib.auth import get_user_model
+from polymorphic.models import PolymorphicModel
+from accountapp.models import Profile
+from ..models import Worldcup, Media
 
-#     def get_absolute_url(self):
-#         return reverse("comment-detail", args=[self.worldcup.pk, self.id])
+
+class Comment(PolymorphicModel):
+
+    worldcup = models.ForeignKey(Worldcup, on_delete=models.CASCADE, verbose_name="월드컵")
+    media = models.ForeignKey(
+        Media, null=True, blank=True, on_delete=models.CASCADE, verbose_name="미디어"
+    )
+    body = models.CharField("댓글 내용", max_length=511)
+    created_at = models.DateTimeField("작성시각", auto_now_add=True)
+    updated_at = models.DateTimeField("수정시각", auto_now=True)
+
+
+class AuthUserComment(Comment):
+
+    writer = models.ForeignKey(
+        get_user_model(), blank=True, on_delete=models.CASCADE, verbose_name="작성자"
+    )
+
+
+class AnonUserComment(Comment):
+
+    anon_nickname = models.CharField(
+        "익명 닉네임",
+        max_length=Profile()._meta.get_field("nickname").max_length,
+        validators=deepcopy(Profile()._meta.get_field("nickname").validators),
+    )
+    anon_password = models.CharField(
+        "익명 패스워드",
+        max_length=get_user_model()()._meta.get_field("password").max_length,
+        validators=deepcopy(get_user_model()()._meta.get_field("password").validators),
+    )
