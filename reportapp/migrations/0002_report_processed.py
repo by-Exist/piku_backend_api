04aaea0bfe1915a702cbfe1399d9f8057281eb29
Jongbeom-Kwon <bolk9652@naver.com>
2021-03-29 17:49:38 +0900

    🔵other: Formatting...
    이런...

--- reportapp/migrations/0002_report_processed.py
+++ reportapp/migrations/0002_report_processed.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reportapp', '0001_initial'),
+        ("reportapp", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='report',
-            name='processed',
-            field=models.BooleanField(default=False, verbose_name='처리 여부'),
+            model_name="report",
+            name="processed",
+            field=models.BooleanField(default=False, verbose_name="처리 여부"),
         ),
     ]
