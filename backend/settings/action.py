091e491a7709069d88a83375c774a55c3054bcfb
Jongbeom-Kwon <bolk9652@naver.com>
2021-04-02 16:58:25 +0900

    ✨feature: Split settings.py
    Github Action 설정이 dev.py에 포함되어 있는 형태가 마음에 들지 않았다.
    그러므로 asgi와 wsgi에서 'GITHUB_WORKFLOW' 환경변수의 유무에 따라 DJANGO_SETTINGS_MODULE을 변경하도록 수정하였다.

--- backend/settings/action.py
+++ backend/settings/action.py
@@ -2,22 +2,11 @@ from .common import *
 import environ
 
 DEBUG = True
-DEV_ENV_PATH = "backend/settings/dev.env"
+ACTION_ENV_PATH = "backend/settings/action.env"
 
 env = environ.Env()
-with open(BASE_DIR / DEV_ENV_PATH) as env_file:
+with open(BASE_DIR / ACTION_ENV_PATH) as env_file:
     environ.Env.read_env(env_file)
-
-INSTALLED_APPS += [
-    "debug_toolbar",
-    "django_extensions",
-    "dummydata",
-]
-
-MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-    *MIDDLEWARE,
-]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
@@ -25,7 +14,7 @@ DEBUG = env("DJANGO_DEBUG")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
-ALLOWED_HOSTS = [*env("DJANGO_ALLOWED_HOSTS").split(" ")]
+ALLOWED_HOSTS = [*env("DJANGO_ALLOWED_HOSTS").split(",")]
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
@@ -42,16 +31,3 @@ INTERNAL_IPS = type(str("c"), (), {"__contains__": lambda *a: True})()
 # LOCAL EMAIL - https://docs.djangoproject.com/en/3.1/topics/email/#file-backend
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR / PROJECT_NAME / "_email"
-
-
-# Dev Drf_spectacular Settings
-SPECTACULAR_SETTINGS = {
-    # Schema metadata
-    "TITLE": "Developer's Swagger",
-    "DESCRIPTION": "개발 용도로 사용되는 Swagger입니다.",
-    "VERSION": "0.0.1",
-    # Useful settings
-    "SERVE_INCLUDE_SCHEMA": False,
-    "COMPONENT_SPLIT_REQUEST": True,
-    "DISABLE_ERRORS_AND_WARNINGS": False,
-}
